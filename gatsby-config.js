64e8928fcc4ba3cdcadbb33f5f4cd8e71b7c5b24
Dmitriy Parhomenko <dmitriyparhomenko140@gmail.com>
2019-08-25 12:17:36 +0300

    Add: scss, main components, page 404 and plugin for images

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,34 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: 'Community lab',
+    author: 'Dmitriy Paromenko'
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`
+      }
+    },
+    'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          'gatsby-plugin-react-helmet',
+        ]
+      }
+    },
+  ]
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,41 @@
-import React from "react"
+import React from 'react'
+import { Link, graphql, useStaticQuery } from 'gatsby'
+import Img from "gatsby-image"
 
-export default () => <div>Community lab</div>
+import Layout from '../components/layout'
+import Head from '../components/head'
+
+const IndexPage = () => {
+
+  const data = useStaticQuery(graphql`
+    query {
+      foto: file(relativePath: { eq: "foto.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000) {
+            ...GatsbyImageSharpFluid_tracedSVG
+          }
+        }
+      }
+      demon: file(relativePath: { eq: "demon.jpeg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000) {
+            ...GatsbyImageSharpFluid_tracedSVG
+          }
+        }
+      }
+    }
+  `)
+
+  console.log(data);
+
+  return (
+    <Layout>
+      <Head title="Hello!" />
+      
+      <Img fluid={data.foto.childImageSharp.fluid} />
+      <Img fluid={data.demon.childImageSharp.fluid} />
+    </Layout>
+  )
+}
+
+export default IndexPage
