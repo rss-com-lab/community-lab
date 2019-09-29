dc6e029f61b738240aef35af7c0fc2cdb787196c
Dmitriy Parhomenko <dmitriyparhomenko140@gmail.com>
2019-09-29 11:49:09 +0300

    feat: adaptive header

--- src/components/header.js
+++ src/components/header.js
@@ -9,7 +9,6 @@ const scrollToElement = require('scroll-to-element');
 const Header = () => {
   
   const handleLinkClick = (e, target) => {
-
     if (typeof window !== undefined) {
       if (window.location.pathname === '/') {
         e.preventDefault()
@@ -20,33 +19,49 @@ const Header = () => {
         })
       }
     }
+  }
 
+  function burgerClick() {
+    let burger = document.querySelector("#burger");
+    let menu = document.querySelector("#menu");
+    
+    if(burger.classList.contains(headerStyles.mobileBurger) && 
+      menu.classList.contains(headerStyles.headerMenu)) {
+      burger.classList.toggle(headerStyles.mobileBurgerActive)
+      menu.classList.toggle(headerStyles.headerMenuActive)
+    }
   }
 
   return (
     <header className={headerStyles.header}>
-      <div className="container">
-        <nav>
-          <ul className={headerStyles.navList}>
-            <li>
-              <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} onClick={e => handleLinkClick(e, '#section1')} to="#section1" > О нас </Link>
-            </li>
-            <li>
-              <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} onClick={e => handleLinkClick(e, '#section2')} to="#section2" > Преимущества </Link>
-            </li>
-            <li>
-              <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} onClick={e => handleLinkClick(e, '#section3')} to="#section3" > Принцип работы </Link>
-            </li>
-            <li>
-              <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} onClick={e => handleLinkClick(e, '#section4')} to="#section4" > Наши проекты </Link>
-            </li>
-            <li>
-              <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} onClick={e => handleLinkClick(e, '#section5')} to="#section5" > Контакты </Link>
-            </li>
-            <Button title="Подать заявку" />
-          </ul>
-        </nav>
+      <div id="menu" className={headerStyles.headerMenu}>
+        <div className="container">
+          <nav>
+            <ul className={headerStyles.navList}>
+              <li>
+                <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} onClick={e => handleLinkClick(e, '#section1')} to="#section1" > О нас </Link>
+              </li>
+              <li>
+                <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} onClick={e => handleLinkClick(e, '#section2')} to="#section2" > Преимущества </Link>
+              </li>
+              <li>
+                <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} onClick={e => handleLinkClick(e, '#section3')} to="#section3" > Принцип работы </Link>
+              </li>
+              <li>
+                <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} onClick={e => handleLinkClick(e, '#section4')} to="#section4" > Наши проекты </Link>
+              </li>
+              <li>
+                <Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} onClick={e => handleLinkClick(e, '#section5')} to="#section5" > Контакты </Link>
+              </li>
+              <Button title="Подать заявку" />
+            </ul>
+          </nav>
+        </div>
       </div>
+      <div id="burger" 
+      className={headerStyles.mobileBurger}
+      onClick={burgerClick}
+      ></div>
     </header>
   )
 }
